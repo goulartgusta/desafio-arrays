f3116064f1b8766000d19a559802e2a544ffdbac
Gustavo Goulart <goulartgusta9@gmail.com>
2025-01-03 12:40:42 -0300

    test: testando etapa 1

--- src/test/java/br/com/almaviva/desafio/array/etapa1/MyMatrizTeste.java
+++ src/test/java/br/com/almaviva/desafio/array/etapa1/MyMatrizTeste.java
@@ -11,46 +11,57 @@ import br.com.almaviva.desafio.array.exceptions.MatrizClassException;
 
 class MyMatrizTeste {
 
-//    private MyMatriz matriz;
-//
-//    @BeforeEach
-//    void setUp() {
-//        matriz = new MyMatriz(3, 3);
-//    }
-//
-//    @Test
-//    void deveriaInserirNumeroComSucesso() {
-//        matriz.inserirNumero(0, 0, 42);
-//        assertEquals(42, matriz.buscarNumero(0, 0));
-//    }
-//
-//    @Test
-//    void deveriaInserirCaractereComSucesso() {
-//        matriz.inserirCaractere(1, 1, 'A');
-//        assertEquals('A', matriz.buscarCaractere(1, 1));
-//    }
-//
-//    @Test
-//    void deveriaRemoverNumeroComSucesso() {
-//        matriz.inserirNumero(2, 2, 99);
-//        matriz.removerNumero(2, 2);
-//        assertNull(matriz.buscarNumero(2, 2));
-//    }
-//
-//    @Test
-//    void deveriaRemoverCaractereComSucesso() {
-//        matriz.inserirCaractere(2, 2, 'Z');
-//        matriz.removerCaractere(2, 2);
-//        assertNull(matriz.buscarCaractere(2, 2));
-//    }
-//
-//    @Test
-//    void deveriaLancarErroAoInserirNumeroForaDosLimites() {
-//        assertThrows(MatrizException.class, () -> matriz.inserirNumero(4, 4, 42));
-//    }
-//
-//    @Test
-//    void deveriaLancarErroAoInserirCaractereForaDosLimites() {
-//        assertThrows(MatrizException.class, () -> matriz.inserirCaractere(4, 4, 'A'));
-//    }
+    private MyMatriz matriz;
+
+    @BeforeEach
+    void setUp() {
+        Character[] caracteres = {'A', 'B', 'C', 'D', 'E'};
+        Integer[] numeros = {1, 2, 3, 4, 5};
+        matriz = new MyMatriz(caracteres, numeros);
+    }
+
+    @Test
+    void deveInserirCaractereComSucesso() {
+        matriz.inserirCaractere(3, 'D');
+        assertEquals('D', matriz.buscarCaractere(3));
+    }
+
+    @Test
+    void deveLancarExcecaoAoInserirCaractereInvalido() {
+        assertThrows(MatrizClassException.class, () -> matriz.inserirCaractere(3, 123));
+    }
+
+    @Test
+    void deveInserirNumeroComSucesso() {
+        matriz.inserirNumero(3, 10);
+        assertEquals(10, matriz.buscarNumero(3));
+    }
+
+    @Test
+    void deveLancarExcecaoAoInserirNumeroInvalido() {
+        assertThrows(MatrizClassException.class, () -> matriz.inserirNumero(3, 'X'));
+    }
+
+    @Test
+    void deveBuscarCaractereComSucesso() {
+        assertEquals('A', matriz.buscarCaractere(0));
+    }
+
+    @Test
+    void deveBuscarNumeroComSucesso() {
+        assertEquals(1, matriz.buscarNumero(0));
+    }
+
+    @Test
+    void deveRemoverCaractereComSucesso() {
+        matriz.removerCaractere(0);
+        assertNull(matriz.buscarCaractere(0));
+    }
+
+    @Test
+    void deveRemoverNumeroComSucesso() {
+        matriz.removerNumero(0);
+        assertNull(matriz.buscarNumero(0));
+    }
+    
 }
