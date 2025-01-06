a9b77fdaf25629556893c4eff43a689e2f6f0712
Gustavo Goulart <goulartgusta9@gmail.com>
2025-01-06 05:10:48 -0300

    test: etapa 1

--- src/test/java/br/com/almaviva/desafio/array/etapa1/MyMatrizTeste.java
+++ src/test/java/br/com/almaviva/desafio/array/etapa1/MyMatrizTeste.java
@@ -1,8 +1,6 @@
 package br.com.almaviva.desafio.array.etapa1;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNull;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -15,53 +13,92 @@ class MyMatrizTeste {
 
     @BeforeEach
     void setUp() {
-        Character[] caracteres = {'A', 'B', 'C', 'D', 'E'};
-        Integer[] numeros = {1, 2, 3, 4, 5};
+        Character[] caracteres = { 'A', 'B', 'C', 'D', 'E' };
+        Integer[] numeros = { 1, 2, 3, 4, 5 };
         matriz = new MyMatriz(caracteres, numeros);
     }
 
     @Test
-    void deveInserirCaractereComSucesso() {
-        matriz.inserirCaractere(3, 'D');
-        assertEquals('D', matriz.buscarCaractere(3));
+    void deveriaInserirCaractereComSucesso() {
+        matriz.inserirCaractere(3, 'Z');
+        assertEquals('Z', matriz.buscarCaractere(3));
     }
 
     @Test
-    void deveLancarExcecaoAoInserirCaractereInvalido() {
-        assertThrows(MatrizClassException.class, () -> matriz.inserirCaractere(3, 123));
+    void deveriaLancarExcecaoAoInserirCaractereInvalido() {
+        assertThrows(MatrizClassException.class, () -> matriz.inserirCaractere(10, 'Z'));
+        assertEquals('A', matriz.buscarCaractere(0));
+        assertEquals('B', matriz.buscarCaractere(1));
     }
 
     @Test
-    void deveInserirNumeroComSucesso() {
+    void deveriaInserirNumeroComSucesso() {
         matriz.inserirNumero(3, 10);
         assertEquals(10, matriz.buscarNumero(3));
     }
 
     @Test
-    void deveLancarExcecaoAoInserirNumeroInvalido() {
-        assertThrows(MatrizClassException.class, () -> matriz.inserirNumero(3, 'X'));
-    }
-
-    @Test
-    void deveBuscarCaractereComSucesso() {
-        assertEquals('A', matriz.buscarCaractere(0));
-    }
-
-    @Test
-    void deveBuscarNumeroComSucesso() {
+    void deveriaLancarExcecaoAoInserirNumeroInvalido() {
+        MatrizClassException exception = assertThrows(MatrizClassException.class, () -> matriz.inserirNumero(10, 20));
         assertEquals(1, matriz.buscarNumero(0));
+        assertEquals(2, matriz.buscarNumero(1));
     }
 
     @Test
-    void deveRemoverCaractereComSucesso() {
+    void deveriaBuscarCaractereComSucesso() {
+        assertEquals('A', matriz.buscarCaractere(0));
+        assertEquals('B', matriz.buscarCaractere(1));
+    }
+
+    @Test
+    void deveriaLancarExcecaoAoBuscarCaractereInvalido() {
+        MatrizClassException exception = assertThrows(MatrizClassException.class, () -> matriz.buscarCaractere(10));
+        assertEquals('C', matriz.buscarCaractere(2));
+    }
+
+    @Test
+    void deveriaBuscarNumeroComSucesso() {
+        assertEquals(1, matriz.buscarNumero(0));
+        assertEquals(2, matriz.buscarNumero(1));
+    }
+
+    @Test
+    void deveriaLancarExcecaoAoBuscarNumeroInvalido() {
+        assertThrows(MatrizClassException.class, () -> matriz.buscarNumero(10));
+        assertEquals(3, matriz.buscarNumero(2));
+    }
+
+    @Test
+    void deveriaRemoverCaractereComSucesso() {
         matriz.removerCaractere(0);
         assertNull(matriz.buscarCaractere(0));
     }
 
     @Test
-    void deveRemoverNumeroComSucesso() {
+    void deveriaLancarExcecaoAoRemoverCaractereInvalido() {
+        MatrizClassException exception = assertThrows(MatrizClassException.class, () -> matriz.removerCaractere(10));
+        assertEquals('A', matriz.buscarCaractere(0));
+    }
+
+    @Test
+    void deveriaRemoverNumeroComSucesso() {
         matriz.removerNumero(0);
         assertNull(matriz.buscarNumero(0));
     }
-    
+
+    @Test
+    void deveriaLancarExcecaoAoRemoverNumeroInvalido() {
+        assertThrows(MatrizClassException.class, () -> matriz.removerNumero(10));
+        assertEquals(1, matriz.buscarNumero(0));
+    }
+
+    @Test
+    void deveriaManterEstadoAposExcecoes() {
+        assertThrows(MatrizClassException.class, () -> matriz.inserirCaractere(10, 'X'));
+        assertEquals('A', matriz.buscarCaractere(0));
+        assertEquals('B', matriz.buscarCaractere(1));
+        assertThrows(MatrizClassException.class, () -> matriz.inserirNumero(10, 100));
+        assertEquals(1, matriz.buscarNumero(0));
+        assertEquals(2, matriz.buscarNumero(1));
+    }
 }
